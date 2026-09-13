8b189c80bd9436e604d42d86cb4db0e7e7827ec6
Sohan.io <sohan@aereo.io>
2026-09-13 09:56:46 +0530

    Implement list class

--- list.py
+++ list.py
@@ -1,2 +1,22 @@
+from card import Card
+from utils import new_id
+
+
 class List:
-    def __init__(self):
+    def __init__(self, name) -> None:
+        self.id = new_id()
+        self.name = name
+        self.cards = []
+
+    def add_card(self, card: Card):
+        self.cards.append(card)
+
+    def remove_card(self, cardId: str):
+        card = self.find_card(cardId)
+        self.cards.remove(card)
+
+    def find_card(self, cardId: str):
+        for card in self.cards:
+            if card.id == cardId:
+                return card
+        return None
