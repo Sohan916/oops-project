30a1884bbbe9b8a2c175ae3f8242edd99508709c
Sohan.io <sohan@aereo.io>
2026-09-13 09:56:19 +0530

    Implement board class

--- board.py
+++ board.py
@@ -5,6 +5,18 @@ class Board:
     def __init__(self) -> None:
         self.Lists = []
 
-    def add_list(self):
+    # List methods.
+    def add_list(self, name):
+        self.Lists.append(List(name))
 
-        self.Lists.append(List())
+    def remove_list(self, list_id):
+        list = self.find_list(list_id)
+        if list is None:
+            raise ValueError("invalid value")
+        self.Lists.remove(list)
+
+    def find_list(self, list_id) -> List | None:
+        for list in self.Lists:
+            if list_id == list.id:
+                return list
+
